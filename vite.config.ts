841b0431b640925cec869e2532d9b14b5d08e330
yanschafer <design@yschafer.ru>
2026-01-29 19:28:16 +0300

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "/kyraweb/",
   plugins: [vue()],
-  server: {
-    port: 5173
-  }
+  server: { port: 5173 }
 });
